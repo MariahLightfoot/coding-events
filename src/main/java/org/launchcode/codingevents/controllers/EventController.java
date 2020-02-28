0ee8af0be670d9de532df3f695319d92aae852bd
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-28 17:06:23 -0600

    added a form at /events/create so that user can create form

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,14 +3,16 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("events")
 public class EventController {
 
-    @GetMapping("events")
+    @GetMapping
     public String displayAllEvents(Model model){
         List<String> events = new ArrayList<>();
         events.add("Code with Pride");
@@ -20,5 +22,10 @@ public class EventController {
         model.addAttribute("events", events);
 
         return"events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreteEventForm(){
+        return "events/create";
     }
 }
