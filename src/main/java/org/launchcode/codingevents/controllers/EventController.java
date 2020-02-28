f3c54cd43fef231e9829fbe51685813292c5dd65
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-28 17:49:22 -0600

    created fragments and practiced using them for static files

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,12 @@
 package org.launchcode.codingevents.controllers;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +15,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Code till Dawn");
-        events.add("Strange Loop");
 
         model.addAttribute("events", events);
 
@@ -26,6 +27,15 @@ public class EventController {
 
     @GetMapping("create")
     public String renderCreteEventForm(){
+
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+
+        events.add(eventName);
+
+        return "redirect:";
     }
 }
