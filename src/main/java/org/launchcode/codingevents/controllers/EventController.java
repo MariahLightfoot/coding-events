0285fd4b0e7e12172a58214c0c9656c96544dbfc
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-03-02 16:49:32 -0600

    created new branch to play with events app more

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,20 +3,24 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 @Controller
 public class EventController {
 
+    HashMap<String, String> events = new HashMap<String, String>();
+
     @GetMapping("events")
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Code till Dawn");
-        events.add("Strange Loop");
+        events.put("StrangeLoop", "Multi-disciplinary conference.");
+        events.put("Code Til Dawn", "Code until the sun comes up!");
+        events.put("Code with Price", "Support for LGBTQ coders.");
 
+//        List<String> events = new ArrayList<>();
+//        events.add("Code with Pride");
+//        events.add("Code till Dawn");
+//        events.add("Strange Loop");
+//
         model.addAttribute("events", events);
 
         return"events/index";
