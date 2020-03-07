8818fb5fafe2c49857431670ac9002681ce9ef99
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-03-07 08:42:44 -0600

    created functionality to delete an event from list

--- src/main/java/org/launchcode/codingevents/data/EventData.java
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -8,26 +8,20 @@ import java.util.Map;
 
 public class EventData {
 
-    // need place to put events, main prop
     private static final Map<Integer, Event> events = new HashMap<>();
 
-    // need behaviors
-    // need to get all events
     public static Collection<Event> getAll(){
         return events.values();
     }
 
-    // get single event
     public static Event getById(int id){
         return events.get(id);
     }
 
-    // add event
     public static void add(Event event){
         events.put(event.getId(), event);
     }
 
-    // remove an event
     public static void remove(int id){
         events.remove(id);
     }
